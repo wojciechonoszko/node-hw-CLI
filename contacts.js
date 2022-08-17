e61a35a7fa7c2461f39a150807d8c85a3a0e1bff
Wojciech <wojciechonoszko@gmail.com>
2022-08-17 17:55:00 +0200

    changes

---
+++ contacts.js
@@ -0,0 +1,19 @@
+const contactsPath = path.join(__dirname, "db/contacts.json");
+
+function listContacts() {
+    // ...twój kod
+  }
+  
+  function getContactById(contactId) {
+    // ...twój kod
+  }
+  
+  function removeContact(contactId) {
+    // ...twój kod
+  }
+  
+  function addContact(name, email, phone) {
+    // ...twój kod
+  }
+
+module.exports = contactsPath;
